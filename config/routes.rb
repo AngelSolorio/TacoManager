caed92560dd8f82d5258dd65b9009de72032dec6
Angel Solorio <angel.solorio@crowdint.com>
2013-08-15 10:48:00 -0500

    New routes and translations added

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 TacoManager::Application.routes.draw do
-  root to: 'home#index'
+  root to: 'profile#show'
+  resources :users
 
   match 'auth/:provider/callback' => 'sessions#create', via: [:get, :post]
   match 'signin' => 'sessions#new', via: [:get, :post], as: :signin
   get 'signup' => 'users#new', as: :signup
   get "signout" => "sessions#destroy", as: :signout
-
-  resources :users
+  get "auth/failure" => "sessions#failure"
 
   if Rails.env.test?
     post '/sessions/create' => 'sessions#create'
   end
+
+  get 'working', :to => 'errors#working'
 end

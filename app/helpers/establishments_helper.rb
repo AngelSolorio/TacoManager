9b9005dfa15d3736edb712a0f4ca3b08e38a9d8d
Angel Solorio <pair+angel.solorio@crowdint.com>
2013-08-22 18:38:52 -0500

    Funtionality to ratting establishments with stars

--- app/helpers/establishments_helper.rb
+++ app/helpers/establishments_helper.rb
@@ -1,11 +1,15 @@
 module EstablishmentsHelper
-  def display_establishments
-    if Establishment.all.empty?
+  def display_establishments(establishments)
+    if establishments.empty?
       content_tag :h3, class: 'gray-label empty-message' do
         t('.empty-list-establishments')
       end
     else
-      render partial: "shared/establishment", collection: Establishment.all, as: :establishment
+      render partial: "shared/establishment", collection: establishments, as: :establishment
     end
+  end
+
+  def render_stars(rating)
+    StarsRenderer.new(rating, self).render_stars
   end
 end
